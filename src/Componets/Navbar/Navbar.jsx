6baa92e0cfd490c99083412798f2dd892edad7ba
Imran ahmad mir <letsmeetimran@gmail.com>
2026-03-10 00:25:45 +0530

    now start workingon shop tab

--- src/Componets/Navbar/Navbar.jsx
+++ src/Componets/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 function Navbar() {
-    const [menu,setmenu] = useState("Shops")
+    const [menu, setmenu] = useState("Shops")
     return (
         <div className='navbar'>
             <div className='nav-logo'>
@@ -11,16 +11,16 @@ function Navbar() {
                 <p>SHOPPER</p>
             </div>
             <ul className='nav-menu'>
-                <li onClick={() =>{setmenu("Shops")}}>Shops{menu === "Shops"?<hr/>: <></> }</li>
-                <li onClick={() =>{setmenu("Men")}}>Men{menu === "Men"?<hr/>: <></> }</li>
-                <li onClick={() =>{setmenu("Women")}}>Women{menu === "Women"?<hr/>: <></> }</li>
-                <li onClick={() =>{setmenu("Kids")}}>Kids{menu === "Kids"?<hr/>: <></> }</li>
+                <li onClick={() => { setmenu("Shops") }}>Shops{menu === "Shops" ? <hr /> : <></>}</li>
+                <li onClick={() => { setmenu("Men") }}>Men{menu === "Men" ? <hr /> : <></>}</li>
+                <li onClick={() => { setmenu("Women") }}>Women{menu === "Women" ? <hr /> : <></>}</li>
+                <li onClick={() => { setmenu("Kids") }}>Kids{menu === "Kids" ? <hr /> : <></>}</li>
             </ul>
             <div className='nav-login-cart'>
                 <button>Login</button>
                 <img src={cart_icon} alt="" />
                 <div className='nav-cart-counter'>
-        0
+                    0
                 </div>
             </div>
         </div>
