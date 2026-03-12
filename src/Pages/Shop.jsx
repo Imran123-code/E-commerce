5ae8f985670a5ee16929d1951bbeaad2098aa3c6
Imran ahmad mir <letsmeetimran@gmail.com>
2026-03-12 23:08:55 +0530

    created hero for data

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,68 +1,73 @@
 import React from 'react'
-import MyImage from "../Componets/Assets/p1_product_i1.png";
+// import MyImage from "../Componets/Assets/p1_product_i1.png";
+import Hero from '../Componets/Hero/Hero';
 function Shop() {
     return (
-        <div className='cointainer' style={{
-             display: "grid",
-        gridTemplateColumns: "1fr 1fr 1fr",
-            gap:'30px',
-            background:'black',
-            color:"white",
-            padding:"40px",
-            marginTop:'30px',
-            marginLeft:'40px',
-            marginRight:'40px',
-            borderx:'1px solid red',
-        }}>
-            <div
-                style={{
-                    width:'300px',
-                    height:'300px',
-                    border:'1px solid white',
-                }}
-            ><img style={{
-                width:"100%"
-            }} src={MyImage} alt='i'/></div>
-             <div
-                style={{
-                    width:'300px',
-                    height:'300px',
-                    border:'1px solid white',
-                }}
-            >
-                <img src={MyImage} alt='i'/>
-            </div>
-             <div
-                style={{
-                    width:'300px',
-                    height:'300px',
-                    border:'1px solid white',
-                }}
-            >
-                <img src={MyImage} alt='i'/>
-            </div>
-             <div
-                style={{
-                    width:'300px',
-                    height:'300px',
-                    border:'1px solid white',
-                }}
-            ><img src={MyImage} alt='i'/></div>
-             <div
-                style={{
-                    width:'300px',
-                    height:'300px',
-                    border:'1px solid white',
-                }}
-            ><img src={MyImage} alt='i'/></div>
-             <div
-                style={{
-                    width:'300px',
-                    height:'300px',
-                    border:'1px solid white',
-                }}
-            ><img src={MyImage} alt='i'/></div>
+        <div>
+            <Hero/>
         </div>
+
+        // <div className='cointainer' style={{
+        //      display: "grid",
+        // gridTemplateColumns: "1fr 1fr 1fr",
+        //     gap:'30px',
+        //     background:'black',
+        //     color:"white",
+        //     padding:"40px",
+        //     marginTop:'30px',
+        //     marginLeft:'40px',
+        //     marginRight:'40px',
+        //     borderx:'1px solid red',
+        // }}>
+        //     <div
+        //         style={{
+        //             width:'300px',
+        //             height:'300px',
+        //             border:'1px solid white',
+        //         }}
+        //     ><img style={{
+        //         width:"100%"
+        //     }} src={MyImage} alt='i'/></div>
+        //      <div
+        //         style={{
+        //             width:'300px',
+        //             height:'300px',
+        //             border:'1px solid white',
+        //         }}
+        //     >
+        //         <img src={MyImage} alt='i'/>
+        //     </div>
+        //      <div
+        //         style={{
+        //             width:'300px',
+        //             height:'300px',
+        //             border:'1px solid white',
+        //         }}
+        //     >
+        //         <img src={MyImage} alt='i'/>
+        //     </div>
+        //      <div
+        //         style={{
+        //             width:'300px',
+        //             height:'300px',
+        //             border:'1px solid white',
+        //         }}
+        //     ><img src={MyImage} alt='i'/></div>
+        //      <div
+        //         style={{
+        //             width:'300px',
+        //             height:'300px',
+        //             border:'1px solid white',
+        //         }}
+        //     ><img src={MyImage} alt='i'/></div>
+        //      <div
+        //         style={{
+        //             width:'300px',
+        //             height:'300px',
+        //             border:'1px solid white',
+        //         }}
+        //     ><img src={MyImage} alt='i'/></div>
+        // </div>
     )
 }
 
