0df22a2c7268170a90757f7ee4549a0b674c22c0
Imran ahmad mir <letsmeetimran@gmail.com>
2026-04-03 22:43:57 +0530

    hero section complete

--- src/Componets/Hero/Hero.jsx
+++ src/Componets/Hero/Hero.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Hero.css'
+import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_img from '../Assets/hero_image.png'
 function Hero() {
   return (
-    <div className='Hero'> 
-      
+    <div className='Hero'>
+      <div className="hero-left">
+        <h2>NEW ARIVAL ITEM</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt='' />
+          </div>
+          <p>collection</p>
+          <p>For Everyone</p>
+        </div>
+        <div className="hero-latest-btn">
+          <div>Latest collection</div>
+          <img src={arrow_icon} alt='' />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_img} alt='' />
+      </div>
     </div>
   )
 }
