f762bead5a525ad3719b8e4c05cbd2bfb0ec7036
Imran ahmad mir <letsmeetimran@gmail.com>
2026-04-04 09:48:48 +0530

    added Get Exclusive Deals container to page

--- src/Componets/Newsletter/Newsletter.jsx
+++ src/Componets/Newsletter/Newsletter.jsx
@@ -3,7 +3,12 @@ import "./Newsletter.css"
 const Newsletter = () => {
   return (
     <div className='newsletter'>
-      
+      <h1>Get Exclusive Deals on your Email</h1>
+      <p>subscribe to our newsletter and never miss an update!</p>
+      <div className="form">
+        <input type="email" placeholder="Enter your email" />
+        <button>Subscribe</button>
+      </div>
     </div>
   )
 }
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -4,6 +4,7 @@ import Hero from '../Componets/Hero/Hero';
 import Popular from '../Componets/Popular/Popular';
 import Offers from '../Componets/Offers/Offers';
 import Newcollection from '../Componets/NewCollections/Newcollection';
+import Newsletter from '../Componets/Newsletter/Newsletter';
 function Shop() { 
     return (
         <div>
@@ -11,6 +12,7 @@ function Shop() {
             <Popular/>
             <Offers/>
             <Newcollection/>
+            <Newsletter/>
         </div>
     )
 }
