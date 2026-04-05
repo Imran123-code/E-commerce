9473c9779b675a1ad34c27d737f37895abcccf8b
Imran ahmad mir <letsmeetimran@gmail.com>
2026-04-05 12:00:39 +0530

    completed login completely

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import './Css/LoginSignup.css'
 function LoginSignup() {
   return (
-    <div>
-      
+    <div className='LoginSignup'>
+      <div className="LoginSignup-cointainer">
+        <h1>Sign up</h1>
+        <div className="LoginSignup-fields">
+          <input type='name' placeholder='enter your name'></input>
+          <input type='Email' placeholder='Enter your email'></input>
+          <input type='password'></input>
+        </div>
+         <button>continue</button>
+        
+         <p className='LoginSignup-login'>Already have an account <span>Login here</span></p>
+        
+         <div className="LoginSignup-conform">
+          <input type="checkbox" name='' id=''/>
+          <p>I agree terms and condition</p>
+         </div>
+      </div>
     </div>
   )
 }
