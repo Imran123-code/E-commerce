4000dd36d0cd19eb5a0b80b19daa408489cb4433
Imran ahmad mir <letsmeetimran@gmail.com>
2026-04-07 18:58:38 +0530

    added Description and Review  dyamically  to each  product

---
+++ src/Componets/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import "./DescriptionBox.css"
+const DescriptionBox = () => {
+    return (
+        <div className="DescriptionBox">
+            <div className="DescriptionBox-nvigator">
+                <div className="DescriptionBox-navbox">Description</div>
+                <div className="DescriptionBox-navbox fade">Review (122)</div>
+            </div>
+            <div className="DescriptionBox-Description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quae suscipit reprehenderit pariatur molestiae deleniti debitis velit quod magni possimus vitae numquam odit omnis, ut officia nobis tempora nulla voluptates?</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut tempora saepe. Quasi eaque corrupti eius adipisci molestias! Saepe recusandae inventore eligendi quod assumenda nesciunt id doloribus accusamus harum repellendus!</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../Componets/Breadcrums/Breadcrum';
 import Productdisplay from '../Componets/ProductDisplay/Productdisplay';
+import DescriptionBox from '../Componets/DescriptionBox/DescriptionBox';
 function Product() {
   const { all_product } = useContext(ShopContext);
   const { productId } = useParams();
@@ -12,6 +13,7 @@ function Product() {
       {/* <Breadcrum product={product}/> */}
       {product && <Breadcrum product={product} />}
       <Productdisplay product={product} />
+      <DescriptionBox/>
     </div>
   )
 }
