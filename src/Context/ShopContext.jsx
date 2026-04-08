c2c0c2c543d841f1458345b7421f759d4f9089d7
Imran ahmad mir <letsmeetimran@gmail.com>
2026-04-08 18:32:34 +0530

    some more bugs to fix

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -5,6 +5,7 @@ export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
     const contextValue = { all_product };
+    
     return(
         <ShopContext.Provider value={contextValue}>
             {props.children}
